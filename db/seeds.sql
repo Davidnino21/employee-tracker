09b54b80d9decab75bdc8823bce9d3bd4643df63
Davidnino21 <davidnino_21@yahoo.com>
2023-12-08 14:11:36 -0800

    created database tables

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO department (name)
+VALUES  ("Engineering"),
+        ("Finances"),
+        ("Web Development"),
+        ("Sales");
+
+INSERT INTO role (title, salary, department_id)
+VALUES  ("Lead Engineer", 120000, 1),
+        ("Accounting", 70000, 2),
+        ("Lead Software Engineer" 160000, 3),
+        ("Sales Lead" 50000, 4),
+        ("Junior Developer" 80000, 3),
+        ("SalesPerson" 40000, 4),
+        ("Account Manager" 120000, 2);
